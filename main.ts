c1b56aca74f9a58a88c59a175aeb4909e2006e76
fonfu6 <113105702+fonfu6@users.noreply.github.com>
2022-09-08 15:33:36 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+for (let index = 0; index <= 4; index++) {
+    let dgt = 0
+    led.plot(randint(0, 4), index)
+    basic.pause(500)
+    led.plot(dgt, index - 1)
+    led.unplot(0, 0)
+}
 basic.forever(function () {
 	
 })
